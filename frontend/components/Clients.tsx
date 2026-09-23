7e153c975caa1ea3fc45f4116c1b3f06fa19f857
mcjosephmoyo7-creator <mcjosephmoyo7@gmail.com>
2026-09-23 11:04:54 +0200

    Update Client.tsx
    git push origin main

--- frontend/components/Clients.tsx
+++ frontend/components/Clients.tsx
@@ -5,11 +5,6 @@ import React from "react";
 const Clients = () => {
   return (
     <section id="testimonials" className="py-20">
-      <h1 className="heading">
-        Kind words from
-        <span className="text-purple"> satisfied clients</span>
-      </h1>
-
       <div className="flex flex-col items-center max-lg:mt-10">
         <div className="flex flex-wrap items-center justify-center gap-4 md:gap-16 max-lg:mt-10">
         </div>
